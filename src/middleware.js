59d46ab86890994e80cfcf896ecf811bac6a30bc
sharonobg <sharon@sharonobrien.com>
2024-02-26 12:35:22 -0500

    cleaning up

--- src/middleware.js
+++
@@ -1,9 +0,0 @@
-export{default} from 'next-auth/middleware'
-
-export const config = {matcher:[
-    "/transaction/:path*",
-    "/addCategory",  
-    "/api/:path*",
-    "/my-spending-plan", 
-]
-}
