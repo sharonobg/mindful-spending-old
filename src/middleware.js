032ac16a38a45e83ebffc83ba074588e64faf681
sharonobg <sharon@sharonobrien.com>
2024-02-27 00:16:19 -0500

    ck middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,10 @@
 export{default} from 'next-auth/middleware'
 
 export const config = {matcher:[
+    "/my-spending-plan",
     "/transaction/:path*",
     "/addCategory",  
     "/api/:path*",
-    "/my-spending-plan", 
+     
 ]
 }
